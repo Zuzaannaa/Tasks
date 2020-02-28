e680bfa7565e5426dd9f46a9d533e14dc006c769
Zuza <zuzalubb@gmail.com>
2020-02-28 11:13:06 -0500

    com

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v/v1/trello")
+@RequestMapping("/v1/trello")
 public class TrelloController {
 
     @Autowired
